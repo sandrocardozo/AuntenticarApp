06c27e6072a06c46db4005b67bb98e7be32ca97d
sandrocardozo <sandroportillo44@gmail.com>
2024-09-02 23:38:33 -0400

    Initial commit

---
+++ app/src/main/java/com/utic/auntenticarapp/Actividad2.java
@@ -0,0 +1,14 @@
+package com.utic.auntenticarapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Actividad2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad2);
+
+        // Puedes agregar aquí cualquier inicialización adicional que necesites
+    }
+}
